bd173599d84c30cb04b960e06b8304012a45a952
Diego Calderon <apahisfree@gmail.com>
2022-06-11 18:24:05 -0500

    Evaluaciones

---
+++ Application/Evaluations/Commands/AddEvaluationToClassroomCommand.cs
@@ -0,0 +1,42 @@
+﻿using ColegioMozart.Application.Evaluations.Dtos;
+
+namespace ColegioMozart.Application.Evaluations.Commands;
+
+public class AddEvaluationToClassroomCommand : IRequest
+{
+    public AddEvaluationResource Resource { get; set; }
+}
+
+public class AddEvaluationToClassroomCommandHandler : IRequestHandler<AddEvaluationToClassroomCommand>
+{
+    private readonly ILogger<AddEvaluationToClassroomCommandHandler> _logger;
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly ICurrentUserService _currentUserService;
+    private readonly ISender _mediator;
+
+    public AddEvaluationToClassroomCommandHandler(
+        ILogger<AddEvaluationToClassroomCommandHandler> logger,
+        IApplicationDbContext context,
+        IMapper mapper,
+        ICurrentUserService currentUserService,
+        ISender mediator)
+    {
+        _logger = logger;
+        _context = context;
+        _mapper = mapper;
+        _currentUserService = currentUserService;
+        _mediator = mediator;
+    }
+
+    public Task<Unit> Handle(AddEvaluationToClassroomCommand request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Crear nueva evaluación");
+
+
+        
+
+
+        throw new NotImplementedException();
+    }
+}
---
+++ Application/Evaluations/Dtos/AddEvaluationResource.cs
@@ -0,0 +1,14 @@
+﻿namespace ColegioMozart.Application.Evaluations.Dtos;
+
+public class AddEvaluationResource
+{
+    public int EvaluationTypeId { get; set; }
+    public string EvaluationName { get; set; }
+    public Guid SubjectId { get; set; }
+    public Guid AcademicPeriodId { get; set; }
+    public Guid ClassRoomId { get; set; }
+    //public Guid TeacherId { get; set; }
+    public decimal Weight { get; set; }
+    public DateTime EvaluationDate { get; set; }
+    public decimal MaximumScore { get; set; }
+}
